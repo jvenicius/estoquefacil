a58c554e50a525aa7c37ead5f1dbf2ef0f009f2e
jvenicius <vidivinny8859@gmail.com>
2024-09-09 13:04:58 -0300

    "Updated Layout component to use react-navigation/native-stack and added Home and Buscar screens"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,25 @@
-import { Stack } from 'expo-router/stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./(tabs)/index";
+import Buscar from "./(tabs)/buscar";
+
+const Stack = createNativeStackNavigator();
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
-    </Stack>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          options={{ title: "Principal" }}
+          component={Home}
+        />
+        <Stack.Screen
+          name="Buscar"
+          component={Buscar}
+          options={{ title: "Autenticação" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
