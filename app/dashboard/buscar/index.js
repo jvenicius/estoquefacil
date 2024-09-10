b39ba1eb1a081b411ff48e666cdeefff632086de
jvenicius <vidivinny8859@gmail.com>
2024-09-10 17:22:50 -0300

    feat: Adicionado a tela de buscar produtos, apenas UI

--- app/dashboard/buscar/index.js
+++ app/dashboard/buscar/index.js
@@ -1,16 +1,51 @@
-import { View, StyleSheet } from "react-native"
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 
 export default function Buscar() {
+
+
     return (
         <View style={styles.container}>
-            Buscar
+            <Text style={styles.inputLabel}>Digite o nome do produto ou SKU</Text>
+            <TextInput style={styles.input} />
+
+            <TouchableOpacity style={styles.searchButton}>
+                <Text style={styles.searchButtonText}>PESQUISAR</Text>
+            </TouchableOpacity>
+
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#C7D1C8",
+        backgroundColor: '#C7D1C8',
+        padding: 16
     },
-})
+
+    inputLabel: {
+        marginBottom: 10,
+        fontSize: 16,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#4B8A96',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 20,
+        backgroundColor: 'white',
+    },
+    searchButton: {
+        backgroundColor: '#4B8A96',
+        paddingVertical: 10,
+        alignItems: 'center',
+        borderRadius: 5,
+        marginTop: 20,
+    },
+    searchButtonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
