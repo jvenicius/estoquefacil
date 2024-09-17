c2c05425968800e833103bb968a513292b8f1b99
joseantonioneto <antonio.souzneto@gmail.com>
2024-09-17 09:07:17 -0300

    Adicionando controle de login por tentativa

--- app/index.js
+++ app/index.js
@@ -5,14 +5,69 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from 'expo-router';
 import { useAuth } from '../hooks/authContext';
 
+const MAX_ATTEMPTS = 3; 
+const BLOCK_TIME = 300000; 
+
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [autoLogin, setAutoLogin] = useState(false);
   const [resultado, setResultado] = useState('');
   const [carregando, setCarregando] = useState(false);
   const router = useRouter();
   const { login } = useAuth();
+
+  useEffect(() => {
+    checkBlocked(); 
+  }, []);
+
+  const checkBlocked = async () => {
+    const lastAttemptTime = await AsyncStorage.getItem('lastAttemptTime');
+    const failedAttempts = await AsyncStorage.getItem('failedAttempts');
+
+    if (lastAttemptTime && failedAttempts) {
+      const currentTime = new Date().getTime();
+      if (currentTime - lastAttemptTime < BLOCK_TIME && failedAttempts >= MAX_ATTEMPTS) {
+        setResultado('*Conta temporariamente bloqueada. Tente novamente mais tarde.');
+      }
+    }
+  };
+
+  const handleLogin = async (email, password) => {
+    setResultado('');
+    setCarregando(true);
+    
+    const lastAttemptTime = await AsyncStorage.getItem('lastAttemptTime');
+    const failedAttempts = await AsyncStorage.getItem('failedAttempts');
+
+    if (lastAttemptTime && failedAttempts) {
+      const currentTime = new Date().getTime();
+      if (currentTime - lastAttemptTime < BLOCK_TIME && failedAttempts >= MAX_ATTEMPTS) {
+        setResultado('*Conta temporariamente bloqueada. Tente novamente mais tarde.');
+        setCarregando(false);
+        return;
+      }
+    }
+
+    const success = await login({ email, password });
+    setCarregando(false);
+
+    if (success) {
+      await AsyncStorage.setItem('usuario', email);
+      await AsyncStorage.removeItem('failedAttempts'); // Limpa as tentativas falhas em caso de sucesso
+      await AsyncStorage.removeItem('lastAttemptTime');
+      router.replace('/dashboard');
+    } else {
+      const newFailedAttempts = (parseInt(failedAttempts) || 0) + 1;
+      await AsyncStorage.setItem('failedAttempts', newFailedAttempts.toString());
+      await AsyncStorage.setItem('lastAttemptTime', new Date().getTime().toString());
+
+      if (newFailedAttempts >= MAX_ATTEMPTS) {
+        setResultado('*Conta temporariamente bloqueada. Tente novamente mais tarde.');
+      } else {
+        setResultado('*As credenciais informadas estão incorretas');
+      }
+    }
+  };
 
   const handleWhatsAppSupport = () => {
     const phoneNumber = "+558596979482"; 
@@ -26,20 +81,6 @@ export default function Login() {
       .catch(() => {
         alert('O WhatsApp não está instalado ou ocorreu um erro ao abrir.');
       });
-  };
-
-  const handleLogin = async (email, password) => {
-    setResultado('');
-    setCarregando(true);
-    const success = await login({ email, password });
-    setCarregando(false);
-    if (success) {
-      await AsyncStorage.setItem('usuario', email);
-      router.replace('/dashboard');
-    }
-    else {
-      setResultado('*As credenciais informadas estão incorretas');
-    }
   };
 
   return (
